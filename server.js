acf44916a4e32e979c22504556768cb4ddcf0126
Aditya Verma <129338439+Aditya7verma@users.noreply.github.com>
2024-08-23 00:39:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,16 +13,7 @@ app.use(cors());
 app.use("/api/portfolio", portfolioRoute);
 
 const port = process.env.PORT || 5000;
-// For Deploying the code
 
-const path = require("path");
-
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
-  });
-}
 app.listen(port, () => {
   console.log("Server is up at Port:- ", port);
 });
